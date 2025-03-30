b73fa0e0fde2aac8fd2db8a250334012183d9613
The-LukeZ <luke.hent3005@gmail.com>
2025-03-30 15:40:00 +0200

    refactor: simplify Svelte configuration by removing unnecessary options

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-  kit: {
-    adapter: adapter({ out: "build", precompress: true }),
-    prerender: {
-      handleHttpError: "warn",
-    },
-  },
+  kit: { adapter: adapter({ out: "build" }) },
   preprocess: vitePreprocess(),
   runes: true,
-  compilerOptions: {
-    runes: true,
-  },
-  vitePlugin: { emitCss: true, inspector: true },
 };
