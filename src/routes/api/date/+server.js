08019e046e47db6923fa0b113f1be1a2f3fb1ff6
The-LukeZ <luke.hent3005@gmail.com>
2025-03-30 15:01:22 +0200

    convert js to ts

--- src/routes/api/date/+server.js
+++
@@ -1,7 +0,0 @@
-import { getDate } from "$lib/utils/utilityFuncs";
-import { json } from "@sveltejs/kit";
-
-/** @type {import('../$types').RequestHandler} */
-export async function GET() {
-  return json(getDate());
-}
